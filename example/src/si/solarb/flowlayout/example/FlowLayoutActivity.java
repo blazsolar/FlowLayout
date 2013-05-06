db7af64b32565b16056a49a5ca4e0e851f3849ce
Blaž Šolar <solarblaz@gmail.com>
2013-05-06 23:04:23 +0200

    Adding basic structure for example app

---
+++ example/src/si/solarb/flowlayout/example/FlowLayoutActivity.java
@@ -0,0 +1,90 @@
+package si.solarb.flowlayout.example;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import si.solarb.flowlayout.FlowLayout;
+
+public class FlowLayoutActivity extends ListActivity {
+
+	ExamplesAdapter mAdapter;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		mAdapter = new ExamplesAdapter(this);
+		setListAdapter(mAdapter);
+    }
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Intent intent = new Intent(this, mAdapter.getItem(position).activityClass);
+		startActivity(intent);
+	}
+
+	private class ExamplesAdapter extends ArrayAdapter {
+
+		private Item[] mItems = {
+//				new Item<FlowLayoutActivity>("Basic sample", FlowLayoutActivity.class)
+		};
+
+		private LayoutInflater mInflater;
+
+		private ExamplesAdapter(Context context) {
+			super(context, 0);
+
+			mInflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount() {
+			return mItems.length;
+		}
+
+		@Override
+		public Item getItem(int position) {
+			return mItems[position];
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if(v == null) {
+				v = mInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+
+			TextView text = (TextView) v.findViewById(android.R.id.text1);
+			text.setText("asd");
+
+			return v;
+		}
+
+		private class Item<T extends Activity> {
+
+			private String name;
+			private Class<T> activityClass;
+
+			public Item(String name, Class<T> activityClass) {
+				super();
+				this.name = name;
+				this.activityClass = activityClass;
+			}
+
+		}
+
+	}
+
+}
